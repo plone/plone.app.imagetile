2264bb312cc43c597ea4d7325ab4f5c1de8ddc44
Rok Garbas <rok@garbas.si>
2012-03-09 10:16:54 +0100

    starting to work on imagetile

--- setup.py
+++ setup.py
@@ -1,46 +1,40 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.0dev'
-
 tests_require = [
     'interlude',
     'plone.app.testing',
     'plone.app.dexterity',
 ]
 
-setup(name='plone.app.imagetile',
-      version=version,
-      description="A tile which displays an image.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='plone.app.imagetile',
+    version=version,
+    description="Image tile for deco UI",
+    long_description=open("README.rst").read(),
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='Thomas Buchberger',
-      author_email='t.buchberger@4teamwork.ch',
-      url='https://github.com/plone/plone.app.imagetile',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'five.grok',
-          'plone.app.tiles',
-          'plone.app.mediarepository',
-          'plone.formwidget.querystring',
-      ],
-      tests_require=tests_require,
-      extras_require=dict(test=tests_require),
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    keywords='plone deco tile',
+    author='Thomas Buchberger',
+    author_email='t.buchberger@4teamwork.ch',
+    url='https://github.com/plone/plone.app.imagetile',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.tiles',
+        'plone.app.deco',
+        ],
+    tests_require=tests_require,
+    extras_require=dict(test=tests_require),
+    entry_points="""
+        # -*- Entry points: -*-
+        [z3c.autoinclude.plugin]
+        target = plone
+        """,
+    )
